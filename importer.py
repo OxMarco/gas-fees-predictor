648e5c55eec863be9c101d39ee158e7284f0ecf7
Matteo Ugolotti <matteo.ugolotti@gmail.com>
2021-03-20 23:04:10 +0000

    Make importer faster

--- importer.py
+++ importer.py
@@ -5,17 +5,9 @@ import json
 import os
 import csv
 
-def save(database, fee, blocknumber):
-    try:
-        conn = sqlite3.connect(database)
-    except Error as e:
-        print('database connection error: '+str(e))
-        sys.exit(-1)
-
+def save(db, fee, blocknumber):
     sql = 'INSERT INTO gas_fees(fee, blocknumber) VALUES('+str(fee)+', '+str(blocknumber)+')'
-    cur = conn.cursor()
-    cur.execute(sql)
-    conn.commit()
+    db.execute(sql)
 
 def main():
 
@@ -29,6 +21,13 @@ def main():
 
     database = cfparser['Server']['database']
 
+    try:
+        conn = sqlite3.connect(database)
+        cur = conn.cursor()
+    except Error as e:
+        print('database connection error: '+str(e))
+        sys.exit(-1)
+
     with open('gasprice.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
@@ -37,8 +36,11 @@ def main():
                 print(f'Column names are {", ".join(row)}')
                 line_count += 1
             else:
-                save(database, row[2], row[1])
+                save(cur, row[2], row[1])
                 line_count += 1
+
+    conn.commit()
+    conn.close()
 
 if __name__ == '__main__':
     main()
